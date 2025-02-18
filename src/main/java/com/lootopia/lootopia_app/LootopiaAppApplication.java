6c9a26c80f9070285583c1cb320f945a9a2a0474
Florian Pescot-Jourdan <florianpescot4@gmail.com>
2025-02-18 17:52:13 +0100

    feat : initialisation spring boot

---
+++ src/main/java/com/lootopia/lootopia_app/LootopiaAppApplication.java
@@ -0,0 +1,13 @@
+package com.lootopia.lootopia_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LootopiaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LootopiaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lootopia/lootopia_app/LootopiaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lootopia.lootopia_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LootopiaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
