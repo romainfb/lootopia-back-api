713da220fffc003e79357ab187753ec82df81862
Thibault Garrigues <107497777+tibopauljack@users.noreply.github.com>
2025-05-02 10:06:59 +0200

    fix: fix cors configuration and qodana support
    Signed-off-by: thibault <thibault.Garrigues@icloud.Com>

--- src/main/java/com/lootopia/lootopia_app/config/SecurityConfig.java
+++ src/main/java/com/lootopia/lootopia_app/config/SecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -16,11 +19,25 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests((authorize) -> authorize
-                    .anyRequest().permitAll()
-            )
-            .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");        // Autorise toutes les origines
+        configuration.addAllowedMethod("*");        // Autorise toutes les méthodes
+        configuration.addAllowedHeader("*");        // Autorise tous les headers
+        configuration.setMaxAge(3600L);             // Cache la configuration CORS pendant 1 heure
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
